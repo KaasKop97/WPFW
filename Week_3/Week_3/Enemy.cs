7232bf79f447510e680c5f3cd3257e01e35b29b1
DuncanK17 <31687727+DuncanK17@users.noreply.github.com>
2020-11-09 15:37:49 +0100

    enemy template

--- Week_3/Week_3/Enemy.cs
+++ Week_3/Week_3/Enemy.cs
@@ -1,13 +1,48 @@
+using System;
+
+
 namespace Week_3
 {
-    abstract class AbstractEnemy {
+    abstract class AbstractEnemy
+    {
+        public int Xpos;
+        public int Ypos;
+        public void move(){
+
+        }
+
     }
 
-    class EnemyLR:AbstractEnemy {
-        
+    public class Enemy:AbstractEnemy{
+        public Enemy(){
+            Xpos = Math.random();
+            Ypos = 0;
+        }
     }
 
-    class EnemyShoot:AbstractEnemy {
+    public class EnemyLR:AbstractEnemy{
+        public EnemyLR(){
+            Xpos = Math.random();
+            Ypos = 0;
+        }
 
+        public void left(){
+
+        }
+
+        public void right(){
+
+        }
+    }
+
+    public class EnemyShoot:AbstractEnemy{
+        public EnemyShoot(){
+            Xpos = Math.random();
+            Ypos = 0;
+        }
+
+        public void shoot(){
+            
+        }
     }
 }
