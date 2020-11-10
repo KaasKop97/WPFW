07869c70090283173454cd9272f0b5beedba3c7e
KaasKop <mitchelvanhamburg@posteo.net>
2020-11-10 14:38:38 +0100

    Oops

---
+++ Week_3/Week_3A/Week_3/Player.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Week_3
+{
+    public class Player
+    {
+        public int Xpos;
+        public int Lives { get; private set; }
+
+        public bool Alive = true;
+
+        public Player()
+        {
+            Xpos = Console.WindowWidth / 2;
+            Lives = 3;
+        }
+
+        public void Left()
+        {
+            Xpos = Math.Max(0, Xpos - 1);
+        }
+
+        public void Right()
+        {
+            Xpos = Math.Min(Console.WindowWidth, Xpos + 1);
+        }
+
+        public void Hit()
+        {
+            Lives -= 1;
+            if (Lives < 1)
+            {
+                Alive = false;
+            }
+        }
+
+        public bool Overlap(int xPosEnemy, int yPosEnemy)
+        {
+            // Player is always at Y pos of window height so we can just check that.
+            if (xPosEnemy == Xpos && yPosEnemy == Console.WindowHeight)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
