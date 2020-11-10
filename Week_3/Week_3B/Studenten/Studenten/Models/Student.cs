960ce17a7a3de0674d29910674b2ec682564dd0a
KaasKop <mitchelvanhamburg@posteo.net>
2020-11-10 14:45:25 +0100

    Setup of students and updated dev launch

---
+++ Week_3/Week_3B/Studenten/Studenten/Models/Student.cs
@@ -0,0 +1,9 @@
+namespace Studenten.Models
+{
+    public class Student
+    {
+        public int StudentNummer { get; set; }
+        public string StudentNaam { get; set; }
+        public string StudentMail { get; set; }
+    }
+}
